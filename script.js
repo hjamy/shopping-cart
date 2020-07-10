596c40a3194df8b4d3bdad9946c7009a3cb63395
Acer <jamy.hasanath03@gmail.com>
2020-07-10 06:02:58 +0600

    tax calculation adjusted, few functions added

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 //phone up button handler
 const upBtnPhone = document.getElementById("plusPhone");
 upBtnPhone.addEventListener("click", function () {
-    const phoneCount = document.getElementById("phoneCount").value;
-    const phoneCountValue = parseInt(phoneCount);
+    const phoneCountValue = productCounterValueFetcher("phoneCount");
     document.getElementById("phoneCount").value = phoneCountValue + 1;
     const phonePrice = document.getElementById("phonePrice").innerHTML;
     const phonePriceValue = parseInt(phonePrice) + 1219;
@@ -10,17 +9,13 @@ upBtnPhone.addEventListener("click", function () {
     const subtotal = document.getElementById("subtotal").innerHTML;
     const subtotalValue = parseFloat(subtotal) + 1219;
     document.getElementById("subtotal").innerHTML = subtotalValue;
-    document.getElementById("tax").innerHTML = subtotalValue * 1 / 10;
-    const tax = document.getElementById("tax").innerHTML;
-    const taxValue = parseFloat(tax);
-    document.getElementById("total").innerHTML = subtotalValue + taxValue;
+    calculateTax_UpadateTotal("tax", "total", subtotalValue);
 })
 
 //phone down button handler
 const downBtnPhone = document.getElementById("minusPhone");
 downBtnPhone.addEventListener("click", function () {
-    const phoneCount = document.getElementById("phoneCount").value;
-    const phoneCountValue = parseInt(phoneCount);
+    const phoneCountValue = productCounterValueFetcher("phoneCount");
     if (phoneCountValue > 1) {
         document.getElementById("phoneCount").value = phoneCountValue - 1;
         const phonePrice = document.getElementById("phonePrice").innerHTML;
@@ -29,20 +24,14 @@ downBtnPhone.addEventListener("click", function () {
         const subtotal = document.getElementById("subtotal").innerHTML;
         const subtotalValue = parseInt(subtotal) - 1219;
         document.getElementById("subtotal").innerHTML = subtotalValue;
-        document.getElementById("tax").innerHTML = subtotalValue * 1 / 10;
-        const tax = document.getElementById("tax").innerHTML;
-        const taxValue = parseFloat(tax);
-        document.getElementById("total").innerHTML = subtotalValue + taxValue;
-
+        calculateTax_UpadateTotal("tax", "total", subtotalValue);
     }
-
 })
 
 //cover up button handler
 const upBtnCover = document.getElementById("plusCover");
 upBtnCover.addEventListener("click", function () {
-    const coverCount = document.getElementById("coverCount").value;
-    const coverCountValue = parseInt(coverCount);
+    const coverCountValue = productCounterValueFetcher("coverCount");
     document.getElementById("coverCount").value = coverCountValue + 1;
     const coverPrice = document.getElementById("coverPrice").innerHTML;
     const coverPriceValue = parseInt(coverPrice) + 59;
@@ -50,17 +39,13 @@ upBtnCover.addEventListener("click", function () {
     const subtotal = document.getElementById("subtotal").innerHTML;
     const subtotalValue = parseInt(subtotal) + 59;
     document.getElementById("subtotal").innerHTML = subtotalValue;
-    document.getElementById("tax").innerHTML = subtotalValue * 1 / 10;
-    const tax = document.getElementById("tax").innerHTML;
-    const taxValue = parseFloat(tax);
-    document.getElementById("total").innerHTML = subtotalValue + taxValue;
+    calculateTax_UpadateTotal("tax", "total", subtotalValue);
 })
 
 //cover down button handler
 const downBtnCover = document.getElementById("minusCover");
 downBtnCover.addEventListener("click", function () {
-    const coverCount = document.getElementById("coverCount").value;
-    const coverCountValue = parseInt(coverCount);
+    const coverCountValue = productCounterValueFetcher("coverCount");
     if (coverCountValue > 1) {
         document.getElementById("coverCount").value = coverCountValue - 1;
         const coverPrice = document.getElementById("coverPrice").innerHTML;
@@ -69,13 +54,21 @@ downBtnCover.addEventListener("click", function () {
         const subtotal = document.getElementById("subtotal").innerHTML;
         const subtotalValue = parseInt(subtotal) - 59;
         document.getElementById("subtotal").innerHTML = subtotalValue;
-        document.getElementById("tax").innerHTML = subtotalValue * 1 / 10;
-        const tax = document.getElementById("tax").innerHTML;
-        const taxValue = parseFloat(tax);
-        document.getElementById("total").innerHTML = subtotalValue + taxValue;
+        calculateTax_UpadateTotal("tax", "total", subtotalValue);
     }
-
 })
+
+function productCounterValueFetcher(productId) {
+    const count = document.getElementById(productId).value;
+    const value = parseInt(count);
+    return value;
+}
+
+function calculateTax_UpadateTotal(taxId, totalId, subtotalValue) {
+    const taxValue = subtotalValue * 1 / 10;
+    document.getElementById(taxId).innerHTML = taxValue;
+    document.getElementById(totalId).innerHTML = subtotalValue + taxValue;
+}
 
 //checkout button handler
 const checkoutBtn = document.getElementById("checkout");
@@ -89,31 +82,27 @@ checkoutBtn.addEventListener("click", function () {
 //item1 remove handler
 const remove1Btn = document.getElementById("remove1");
 remove1Btn.addEventListener("click", function () {
-    const item1 = document.getElementById("item1");
     const phonePrice = document.getElementById("phonePrice").innerHTML;
     const phonePriceValue = parseInt(phonePrice);
     const subtotal = document.getElementById("subtotal").innerHTML;
     const subtotalValue = parseInt(subtotal);
     const subtotalValueAfterRemoval = subtotalValue - phonePriceValue;
     document.getElementById("subtotal").innerHTML = subtotalValueAfterRemoval;
-    const taxValue = subtotalValueAfterRemoval * 1 / 10;
-    document.getElementById("tax").innerHTML = taxValue;
-    document.getElementById("total").innerHTML = subtotalValueAfterRemoval + taxValue;
+    calculateTax_UpadateTotal("tax", "total", subtotalValueAfterRemoval);
+    const item1 = document.getElementById("item1");
     item1.style.display = "none";
 })
 
 //item2 remove handler
 const remove2Btn = document.getElementById("remove2");
 remove2Btn.addEventListener("click", function () {
-    const item2 = document.getElementById("item2");
     const coverPrice = document.getElementById("coverPrice").innerHTML;
     const coverPriceValue = parseInt(coverPrice);
     const subtotal = document.getElementById("subtotal").innerHTML;
     const subtotalValue = parseInt(subtotal);
     const subtotalValueAfterRemoval = subtotalValue - coverPriceValue;
     document.getElementById("subtotal").innerHTML = subtotalValueAfterRemoval;
-    const taxValue = subtotalValueAfterRemoval * 1 / 10;
-    document.getElementById("tax").innerHTML = taxValue;
-    document.getElementById("total").innerHTML = subtotalValueAfterRemoval + taxValue;
+    calculateTax_UpadateTotal("tax", "total", subtotalValueAfterRemoval);
+    const item2 = document.getElementById("item2");
     item2.style.display = "none";
 })
